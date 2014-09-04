74113033f92281ec61d73ba74609c776e30bd9b2
Simon Moller <Simon@Simons-MacBook-Air.local>
2014-09-04 14:08:49 +1000

    First draft

--- src/maze_explore_laser.py
+++ src/maze_explore_laser.py
@@ -5,7 +5,9 @@ roslib.load_manifest("turtlebot_explore")
 import rospy
 import numpy as np
 
+
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
 
 class MazeExplorer(object):
     def __init__(self):
@@ -13,9 +15,13 @@ class MazeExplorer(object):
         self.angles = []
         self.angle_min, self.angle_max, self.angle_increment = 0, 0, 0
         self._spacing = 0.3
+        self._angle_margin = np.deg2rad(10)
+        self._speed = 2
+
 
         rospy.init_node("maze_explorer", anonymous=True)
         self.sub = rospy.Subscriber("scan", LaserScan, self.scan_callback)
+        self.pub = rospy.Publisher('cmd_vel_mux/input/navi', Twist)
         rospy.spin()   # ??? do we want to run nodes separately so they run all the time?
 
     def scan_callback(self, data):
@@ -24,21 +30,30 @@ class MazeExplorer(object):
         self.ranges = [x for x in ranges if x > 0]
         self.angles = [angle_min + i * angle_increment for i, j in enumerate(ranges) if j > 0]
         self.xy = [to_xy(r, a) for r, a in zip(self.ranges, self.angles)]
-
-        closest = self.closest_point()
-
-        if closest[0] > 0:
-            x = closest[0]  
-        elif closest[0] < 0:
-        
+    
+        closest_point = self.closest_point()
+        if closest_point[1] > 0:
+            rotation = np.deg2rad(-90) + closest_point[1]
         else:
-        
+            rotation = np.deg2rad(90) + closest_point[1]
 
+        if numpy.fabs(rotation) < self._angle_margin:
+            rotation = 0
 
+        twist = Twist()
+
+        if rotation == 0:
+            #drive
+            twist.linear.x = self._speed
+        else:
+            #rotate
+            twist.angular.z = rotation     # speed, rotation
+        self.pub.publish(twist)
+            
 
     def closest_point(self):
-        index, range = min(enumerate(self.ranges), key=lambda x: x[1])
-        return to_xy(range, self.angles[index])
+        index, distance = min(enumerate(self.ranges), key=lambda x: x[1])
+        return (distance, self.angles[index])
 
     def segmentise(self):
         d_thd = 0.05
