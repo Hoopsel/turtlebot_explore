e4d53497d3619d094563ade5a6197dbda0b908b5
Simon Moller <Simon@Simons-MacBook-Air.local>
2014-09-04 16:36:41 +1000

    Second Draft

--- src/maze_explore_laser.py
+++ src/maze_explore_laser.py
@@ -14,10 +14,12 @@ class MazeExplorer(object):
         self.ranges = []
         self.angles = []
         self.angle_min, self.angle_max, self.angle_increment = 0, 0, 0
-        self._spacing = 0.5
+        self._spacing = 0.25
         self._angle_margin = np.deg2rad(20)
         self._speed = 2
         self._last_rotation = 0
+        self._front_angle = np.deg2rad(10)
+        self._right_turn = True
 
 
         rospy.init_node("maze_explorer", anonymous=True)
@@ -32,6 +34,7 @@ class MazeExplorer(object):
         self.angles = [angle_min + i * angle_increment for i, j in enumerate(ranges) if j > 0]
         self.xy = [to_xy(r, a + np.pi/2) for r, a in zip(self.ranges, self.angles)]
 
+        """
         segments = self.segmentise()
         for segment in segments:
             if self.is_wall(segment):
@@ -39,12 +42,43 @@ class MazeExplorer(object):
             else:
                 plt.scatter(*zip(*segment), c="blue")
         plt.show()
+        """
     
         # check for more busy side
         left = sum(x for i, x in enumerate(self.ranges) if self.angles[i] < 0)
         right = sum(x for i, x in enumerate(self.ranges) if self.angles[i] > 0)
 
         closest_point = self.closest_point()
+        point_in_front = self.point_in_front()
+
+        twist = Twist()
+        speed = 0
+        rotation = 0
+        # if on right, nothing in front go forward
+        if not closest_point:
+            if self._right_turn:
+                self._right_turn = False
+                rotation = np.deg2rad(30)
+            else:
+                self._right_turn = True
+                speed = self._speed
+
+        elif closest_point and (not point_in_front or point_in_front[0] > self._spacing):
+            closest_point_xy = to_xy(closest_point)
+            rotation = np.deg2rad(-90) + closest_point[1]
+            speed = 0
+            if np.fabs(rotation) < self._angle_margin:
+                rotation = 0
+                speed = self._speed
+        # if some in front & on right, turn left
+        elif point_in_front and point_in_front[0] <= self._spacing:
+            rotation = np.deg2rad(90) + point_in_front[1]
+             
+
+        self.pub.publish(twist)
+
+
+        """
         if closest_point[1] > 0 or right > left:
             rotation = np.deg2rad(-90) + closest_point[1]
         else:
@@ -73,11 +107,15 @@ class MazeExplorer(object):
                 #self.pub.publish(twist)
                 rospy.sleep(0.1)
         self._last_rotation = rotation
+        """
             
 
     def closest_point(self):
-        index, distance = min(enumerate(self.ranges), key=lambda x: x[1])
-        return (distance, self.angles[index])
+        try:
+            index, distance = min((i,x) for i, x enumerate(self.ranges) if to_xy(distance, self.angles[index])[0] > 0, key=lambda x: x[1])
+            return (distance, self.angles[index])
+        except ValueError:
+            return False 
 
     def segmentise(self):
         d_thd = 0.05
@@ -104,8 +142,18 @@ class MazeExplorer(object):
         xc, yc = segment[-1]
         return (xa*(yb - yc) + xb*(yc - ya) + xc*(ya- yb)) < some_thd
 
+    def point_in_front(self):
+        try:
+            index, distance = min((i,x) for i, x in enumerate(self.ranges) if np.fabs(self.angles[i]) < self._angle_margin, key=lambda x: x[1])
+            return (distance, self.angles[index])
+        except ValueError:
+            return False 
+
+
     def _distance(self, x, y):
         return np.linalg.norm(np.array(x) - np.array(y))
+
+        
 
 def to_xy(r, a):
     return (r*np.cos(a), r*np.sin(a))
