15d5039c5926e88c65a609cba2e90750cc2e7da2
Simon Moller <Simon@Simons-MacBook-Air.local>
2014-09-08 15:01:30 +1000

    Added maze solver

---
+++ src/maze_solver.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python2
+import roslib
+roslib.load_manifest("turtlebot_explore")
+import rospy
+import numpy as np
+import tf
+
+from nav_msgs.msg import OccupancyGrid
+from turtlebot_explore.msg import Beacon
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from grid_search import grid_search
+
+
+
+class MazeSolver(object):
+
+    def __init__(self):
+        self.map_sub = rospy.Subscriber('/slamGrid', OccupancyGrid, self._update_grid, queue_size = 1)
+        self.beacon_sub = rospy.Subscriber('/turtlebot_beacon', Beacon, self._beacon_found, queue_size = 1) 
+        self.pub = rospy.Publisher('cmd_vel_mux/input/navi', Twist)
+        self.explore_pub = rospy.Publisher('/turtle_cmd', String, queue_size=1)
+        self._occ_grid = None
+        self.map_position = None
+        self.beacons = {} #store as beacon colours : position -> (pink,blue) : (27,32)
+        self.goals = []
+        self.heading = None
+        self._angle_margin = np.deg2rad(10) 
+        self._speed = 2
+        self._unpack_goals()
+    
+    def _unpack_goals(self):
+        beacons = rospy.get_param('beacons')
+        for i in range(len(beacons)):
+            key = "beacon%d" % i
+            beacon = beacons[key]
+            self.goals.append((beacon["top"], beacon["bottom"]))
+
+    # listen for map updates
+    def _update_grid(self, data):
+        self._occ_grid = data
+
+    def update_position(self, listener):
+        try:
+            (position, quaternion) = listener.lookupTransform('/slamGrid', '/base_link', rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            return
+
+        # Find position in map
+        self.map_position = ((current_position[0] - self._occ_grid.origin[0]) / self._occ_grid.resolution,
+                            (current_position[1] - self._occ_grid.origin[1]) / self._occ_grid.resolution)
+        
+        
+        (roll, pitch, yaw) = tf.transformations.euler_from_quaternion(quaternion)
+        self.heading = yaw
+
+
+    # listen for beacon found
+    def _beacon_found(self, data):
+          
+        # figure out beacon position and target position for beacon 
+        rotation = -self.heading 
+
+        # rotate point so it is as if robot is facing north
+        x_dist = (data.x * np.cos(rotation) - data.y * np.sin(rotation)) / self._occ_grid.resolution
+        y_dist = (data.x * np.sin(rotation) + data.y * np.cos(rotation)) / self._occ_grid.resolution
+
+        x = self.map_position[0] + x_dist
+        y = self.map_position[1] + y_dist
+
+        self.beacons[(data.top_colour, data.bot_colour)] = (x,y) 
+
+        # check if last beacon found
+        if not set(self.goals).issubset(self.beacons.keys()):
+            return
+        
+        # stop explore
+        self.stop_explore()
+        # solve
+        self.solve_maze()
+    
+    
+    def start_explore(self):
+        self.explore_pub.publish("explore") # some duplication here, not sure py version of headers are
+
+    def stop_explore(self):
+        self.explore_pub.publish("stop explore") 
+
+    def solve_maze():
+
+        for goal in self.goals:
+            position = self.beacons[goal]
+            path = grid_search(self._occ_grid.data, self._occ_grid.width, self._occ_grid.height, self.map_position, position)
+            for coord in path:
+                move(*coord)
+            print "Reached goal " + goal
+        print "Finished maze"
+
+    def move(to_x, to_y):
+
+        while np.fabs(self.map_position[0] - to_x) >= 1 or np.fabs(self.map_position[y] - to_y) >= 1:
+            partial_move = self._partial_move(to_x, to_y)
+            self.pub.publish(partial_move)
+
+
+    def _partial_move(to_x, to_y):
+        required_heading = np.atan2(to_y - self.map_position[1], 
+                                    to_x - self.map_position[0])
+        rotation = required_heading - self.heading
+
+        if np.fabs(rotation) < self._angle_margin:
+            rotation = 0
+
+        speed = 0 if rotation > 0 else self._speed 
+
+        twist = Twist()
+        twist.linear.x = speed 
+        twist.angular.z = rotation
+        return twist
+
+         
+def main():
+    rospy.init_node('turtlebot_maze_solver')
+    maze_solver = MazeSolver()
+    maze_solver.start_explore()
+    listener = tf.TransformListener()
+    rospy.Rate(10.0)
+    maze_solver.update_position(listener)
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
