2f6726edb03342db28291362cbb67a2d3c411d65
andrew <andrew.mcgill@hotmail.com>
2014-09-08 14:02:09 +1000

    msg gen, changes in cv detector

--- src/pole_detector.py
+++ src/pole_detector.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python2
+import sys
 import roslib
 import rospy
+roslib.load_manifest('turtlebot_explore')
+
+import numpy as np
 import cv2
-import cv
-import sys
+from cv_bridge import CvBridge, CvBridgeError
+
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
+#from turtlebot_explore.msg import Beacon
+
+from scan_detector import ScanDetector
 
 class PoleDetector:
 
@@ -14,139 +20,76 @@ class PoleDetector:
         self.image_sub = rospy.Subscriber("/camera/rgb/image_color",Image, self.callback, queue_size = 1)
         self.bridge = CvBridge()
         self.cv_image = None
-        #capture = imgage_sub <- this doesnt work. image is in data var in the callback
-        #Creates the visuals for us, don't need this for the robots internal workings
-        # image created in bridge in callback.
-        """frame = cv.QueryFrame(capture)
-        frame_size = cv.GetSize(frame)
-        test=cv.CreateImage(cv.GetSize(frame),8,3)
-        img2=cv.CreateImage(cv.GetSize(frame),8,3)
-        """
-        cv.NamedWindow("Real",0)
-        cv.NamedWindow("Threshold",0)
+        self.scanner = ScanDetector()
+#        self.pub = rospy.Publisher('turtlebot_beacon', Beacon)
 
     def callback(self, data):
-        print "callback"
         try:
             self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8") # image created here
         except CvBridgeError as e:
-            print e
+            print e 
 
-        # why run ten times?
-        
-        i=0
-        while(i<10):
-            yellowY = colourWorkings('y')
-            i = i+1
-        i=0
-        while(i<10):
-            blueY = colourWorkings('b')
-            i = i+1
-        i=0
-        while(i<10):
-            greenY =colourWorkings('g')
-            i = i+1
-        i=0
-        while(i<10):
-            pinkY = colourWorkings('p')
-            i = i+1
+        self.process_image()
 
-        #   ALL OF THIS IS FOR TESTING, SO THIS UGLY CODE IS GOIN AWAY!
-        if checkYcoords(yellowY,pinkY):
-            cv.Line(result,(300,50),(300,250),(0,255,255),4,8,0)
-            cv.Line(result,(300,250),(300,450),(180,105,255),4,8,0)
-        if checkYcoords(pinkY,yellowY):
-            cv.Line(result,(350,50),(350,250),(180,105,255),4,8,0)
-            cv.Line(result,(350,250),(350,450),(0,255,255),4,8,0)
-        if checkYcoords(pinkY,blueY):
-            cv.Line(result,(400,50),(400,250),(180,105,255),4,8,0)
-            cv.Line(result,(400,250),(400,450),(255,0,0),4,8,0)
-        if checkYcoords(blueY,pinkY):
-            cv.Line(result,(450,50),(450,250),(255,0,0),4,8,0)
-            cv.Line(result,(450,250),(450,450),(180,105,255),4,8,0)
-        if checkYcoords(yellowY,greenY):
-            cv.Line(result,(250,50),(250,250),(0,255,255),4,8,0)
-            cv.Line(result,(250,250),(250,450),(0,255,0),4,8,0)
-        if checkYcoords(greenY,yellowY):
-            cv.Line(result,(200,50),(200,250),(0,255,0),4,8,0)
-            cv.Line(result,(200,250),(200,450),(0,255,255),4,8,0)
-        if checkYcoords(pinkY,greenY):
-            cv.Line(result,(150,50),(150,250),(180,105,255),4,8,0)
-            cv.Line(result,(150,250),(150,450),(0,255,0),4,8,0)
-        if checkYcoords(greenY,pinkY):
-            cv.Line(result,(100,50),(100,250),(0,255,0),4,8,0)
-            cv.Line(result,(100,250),(100,450),(180,105,255),4,8,0)
-        cv.ShowImage("RESULT",result)
-        if Cpressed():
-            break
-        result=cv.CreateImage(cv.GetSize(frame),8,3)
-        cv.NamedWindow("RESULT",0)
+    def process_image(self):
+        yellowBox = self.find_bounding('y')
+        blueBox = self.find_bounding('b')
+        greenBox = self.find_bounding('g')
+        pinkBox = self.find_bounding('p')
 
-    def getthresholdedimg(im,whichColor):
-        # Convert from RGB to HSV
-        imghsv=cv.CreateImage(cv.GetSize(im),8,3)
-        cv.CvtColor(im,imghsv,cv.CV_BGR2HSV)
-        # Image to be returned
-        imgthreshold=cv.CreateImage(cv.GetSize(im),8,1)
-        # Make the thresholded image
-        img=cv.CreateImage(cv.GetSize(im),8,1)
-        if whichColor == 'y':
-            cv.InRangeS(imghsv,cv.Scalar(0,200,200),cv.Scalar(225,255,255),img) # Range of Yellow colours
-            cv.Add(imgthreshold,img,imgthreshold)
-        if whichColor == 'b':
-            cv.InRangeS(imghsv,cv.Scalar(30,80,120),cv.Scalar(120,205,255),img) # Range of Blue colours
-            cv.Add(imgthreshold,img,imgthreshold)
-        if whichColor == 'g':
-            cv.InRangeS(imghsv,cv.Scalar(50,85,55),cv.Scalar(120,240,180),img)  # Range of Green colours
-            cv.Add(imgthreshold,img,imgthreshold)
-        if whichColor == 'p':
-            cv.InRangeS(imghsv,cv.Scalar(140,100,130),cv.Scalar(255,180,240),img)   # Range of Pink colours
-            cv.Add(imgthreshold,img,imgthreshold)
-        return imgthreshold
+        im = self.cv_image        # show result
+        found = False             # condition for if beacon is seen?
+        for box in [yellowBox, blueBox, greenBox, pinkBox]:
+            x, y, w, h = box
+            if w < 100 and h < 100:
+                cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                print 'box found!'
+                found = True
 
-    def colourWorkings(color):
-    #   THE SAME BASIS FOR ALL COLOUR
-        color_image = self.cv_image # < this may be none! must handle
-        imdraw=cv.CreateImage(cv.GetSize(frame),8,3)
-        cv.SetZero(imdraw)
-        cv.Flip(color_image,color_image,1)
-        cv.Smooth(color_image, color_image, cv.CV_GAUSSIAN, 3, 0)
-    #   BASED ON THE COLOR SPESIFIED
-        imgCOLORthresh=getthresholdedimg(color_image,color)
-        cv.Erode(imgCOLORthresh,imgCOLORthresh,None,3)
-        cv.Dilate(imgCOLORthresh,imgCOLORthresh,None,10)
-        img2=cv.CloneImage(imgCOLORthresh)
-        storage = cv.CreateMemStorage(0)
-        contour = cv.FindContours(imgCOLORthresh, storage, cv.CV_RETR_CCOMP, cv.CV_CHAIN_APPROX_SIMPLE) 
-        while contour:
-    #       Find bounding rectangle
-            bound_rect = cv.BoundingRect(list(contour))
-            pt1 = (bound_rect[0], bound_rect[1])
-            pt2 = (bound_rect[0] + bound_rect[2], bound_rect[1] + bound_rect[3])
-    #       Find the boundRect center
-            posx=cv.Round((pt1[0]+pt2[0])/2)
-            posy=cv.Round((pt1[1]+pt2[1])/2)
-    #       the y coord's what's compared
-            centerspot = [posx,posy]
-    #       Draw circle in the boundRect center for visual aid
-            cv.Circle(color_image,(posx,posy),7,(200,200,200),2)
-            cv.Circle(color_image,(posx,posy),10,(80,80,80),2)
-    #       Visuals for us, for robot it is return statement
-            cv.ShowImage("Real",color_image)
-            cv.ShowImage("Threshold",img2)
-            return posy
+        if found:
+            cv2.imshow("show", im)
+            cv2.waitKey()
+            pole_pos = self.scanner.get_circle_pos()
+            if pole_pos is not None:
+                beacon_found(pole_pos, 'b', 'g') # replace with actual top, bot colour
 
-    def Cpressed():
-        if cv.WaitKey(33)==1048603:
-            cv.DestroyWindow("Real")
-            cv.DestroyWindow("Threshold")
-            return True
+    def set_image(self, im):
+        self.cv_image = im
 
-    def checkYcoords(spottop,spotbottom):
-        if spottop > spotbottom:
-            return True
-        return False
-    
+    def beacon_found(self, pos, top_colour, bot_colour):
+#        msg = Beacon()
+        msg.top_colour = top_colour
+        msg.bot_colour = bot_colour
+        msg.x = pos[0]
+        msg.y = pos[1]
+        self.pub.publish(msg)
+
+    def find_bounding(self, colour):
+        # set colour range based on colour specified
+        if colour == 'y':
+            colour_min, colour_max = np.array([0,200,200], np.uint8), np.array([225,255,255], np.uint8)
+        elif colour == 'b':
+            colour_min, colour_max = np.array([30,80,120], np.uint8), np.array([120,205,255], np.uint8)
+        elif colour == 'g':
+            colour_min, colour_max = np.array([50,85,55], np.uint8), np.array([120,240,180], np.uint8)
+        elif colour == 'p':
+            colour_min, colour_max = np.array([140,100,130], np.uint8), np.array([255,180,240], np.uint8)
+        else:
+            return None
+
+        im = self.cv_image # < this may be none! must handle
+        hsv_img = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+        frame_thd = cv2.inRange(hsv_img, colour_min, colour_max)
+        ret, thd = cv2.threshold(frame_thd, 127, 255, 0)
+        contours, hierarchy = cv2.findContours(thd, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        # find largest contour in set
+        areas = [cv2.contourArea(c) for c in contours]
+        largest = np.argmax(areas)
+        contour = contours[largest]
+
+        bounding = cv2.boundingRect(contour)
+        return bounding
 
 def main(args):
     detector = PoleDetector()
@@ -158,4 +101,17 @@ def main(args):
 
     cv2.destroyAllWindows()
 
-if __name__ == '__main__': main(sys.argv)
+def test():
+    pd = PoleDetector()
+    im = cv2.imread('/home/andrew/ros/indigo/src/turtlebot_explore/assets/image.jpg')
+    #im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
+    pd.set_image(im)
+    pd.process_image()
+
+
+    cv2.imshow("show", im)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__': test()
